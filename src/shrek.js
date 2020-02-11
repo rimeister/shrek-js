b0917a2ca74b7d79cab8d5d51eb3bc766f7e8b6d
Riley Boyd <contact@rileyboyd.com>
2020-02-10 21:36:14 -0500

    Adding three.js init/start code

--- src/shrek.js
+++ src/shrek.js
@@ -1,3 +1,5 @@
+import * as THREE from 'nodule_modules/three.module.js';
+
 class Swamp {
 
 	// A "Swamp" will basically just be a three.js world, or something
@@ -9,7 +11,34 @@ class Swamp {
 
 	// Initializing the swamp sets up the world, camera, etc.
 	init() {
+
+		// Might need to add 'this' etc. later, just pasted from three.js docs
+	    camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 10 );
+	    camera.position.z = 1;
+	 
+	    scene = new THREE.Scene();
+	 
+	    geometry = new THREE.BoxGeometry( 0.2, 0.2, 0.2 );
+	    material = new THREE.MeshNormalMaterial();
+	 
+	    mesh = new THREE.Mesh( geometry, material );
+	    scene.add( mesh );
+	 
+	    renderer = new THREE.WebGLRenderer( { antialias: true } );
+	    renderer.setSize( window.innerWidth, window.innerHeight );
+	    document.body.appendChild( renderer.domElement );
 		
+	}
+
+	start() {
+
+	    requestAnimationFrame( animate );
+	 
+	    mesh.rotation.x += 0.01;
+	    mesh.rotation.y += 0.02;
+	 
+	    renderer.render( scene, camera );
+
 	}
 
 }
